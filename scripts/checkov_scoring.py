b1775e6eeec8c85d45d42dbb59590ae900e4e67b
Michael McClelland <30412622+Michael-McClelland@users.noreply.github.com>
2024-03-05 17:33:08 -0500

    Create checkov_scoring.py

---
+++ scripts/checkov_scoring.py
@@ -0,0 +1,31 @@
+import os
+import json
+import matplotlib.pyplot as plt
+f = open('results_json.json')
+data = json.load(f)
+expected_failures = 8
+actual_failures = 0
+expected_passes = 9
+actual_passes = 0
+for i in data['results']['passed_checks']:
+  print(i['check_id'])
+  print(i['check_result']['result'])
+  print(i['resource'])
+  actual_passes = actual_passes + 1
+for i in data['results']['failed_checks']:
+  print(i['check_id'])
+  print(i['check_result']['result'])
+  print(i['resource'])
+  actual_failures = actual_failures + 1
+f.close()
+
+
+labels = 'expected_failures', 'actual_failures', 'expected_passes', 'actual_passes'
+sizes = [expected_failures,actual_failures,expected_passes,actual_passes]
+
+fig, ax = plt.subplots()
+ax.pie(sizes, labels=labels, autopct='%1.1f%%')
+if not os.path.exists('./images'):
+  os.makedirs('./images')
+fig.savefig(os.getcwd() + "/images/checkov_piechart.png")
+plt.show()
